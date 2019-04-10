2d216213d8bac4b42d012a165401c209c591ad6f
NewLanded <l1141041@163.com>
2019-04-10 13:27:14 +0800

    add function get qfq point data

--- util/util_data/security_data.py
+++ util/util_data/security_data.py
@@ -23,6 +23,18 @@ class SecurityData:
 
         return security_point_data
 
+    def get_qfq_security_point_data(self, ts_code, start_date, end_date):
+        sql = """
+        select `ts_code`, `trade_date`, `open`, `high`, `low`, `close`, `pre_close`, `change`, `pct_chg`, `vol`, `amount` from qfq_security_point_data 
+        where ts_code = %(ts_code)s and trade_date between %(start_date)s and %(end_date)s
+        """
+        args = {"ts_code": ts_code, "start_date": start_date, "end_date": end_date}
+        security_point_data = pd.read_sql_query(sql, engine, params=args)
+        security_point_data.set_index(security_point_data["trade_date"], inplace=True)
+        security_point_data = security_point_data.sort_index()
+
+        return security_point_data
+
     def get_security_daily_basic_data(self, ts_code, start_date, end_date):
         sql = """
         select ts_code, trade_date, close, turnover_rate, turnover_rate_f, volume_ratio, pe, pe_ttm, pb, ps, ps_ttm, total_share, float_share, free_share, total_mv, circ_mv from security_daily_basic 
