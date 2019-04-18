b0d778dee76f7ef58b8a4acf33bfd8f392969ff8
flydedog <l1141041@163.com>
2019-04-18 22:09:17 +0800

    add idea 8

--- idea/penetrate_lower_bband/analyst_bbond_result.py
+++ idea/penetrate_lower_bband/analyst_bbond_result.py
@@ -1,5 +1,5 @@
 import datetime
-
+import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
@@ -121,57 +121,63 @@ def idea_07(data):
     """
     max_depth_map_list = [
         {
-            "next_2_day": 12,
-            "next_3_day": 12,
+            "next_2_day": 1,
+            "next_3_day": 13,
             "next_4_day": 12,
-            "next_5_day": 12,
-            "next_6_day": 12,
-            "next_7_day": 12,
-            "next_8_day": 12,
-            "next_9_day": 12,
-            "next_10_day": 12
+            "next_5_day": 9,
+            "next_6_day": 7,
+            "next_7_day": 8,
+            "next_8_day": 8,
+            "next_9_day": 7,
+            "next_10_day": 9
         },
         {
-            "next_2_day": 12,
-            "next_3_day": 12,
-            "next_4_day": 12,
-            "next_5_day": 12,
-            "next_6_day": 12,
-            "next_7_day": 12,
-            "next_8_day": 12,
-            "next_9_day": 12,
-            "next_10_day": 12
+            "next_2_day": 8,
+            "next_3_day": 6,
+            "next_4_day": 1,
+            "next_5_day": 6,
+            "next_6_day": 2,
+            "next_7_day": 3,
+            "next_8_day": 2,
+            "next_9_day": 3,
+            "next_10_day": 2
         },
         {
-            "next_2_day": 12,
-            "next_3_day": 12,
-            "next_4_day": 12,
-            "next_5_day": 12,
-            "next_6_day": 12,
-            "next_7_day": 12,
-            "next_8_day": 12,
-            "next_9_day": 12,
-            "next_10_day": 12
+            "next_2_day": 15,
+            "next_3_day": 2,
+            "next_4_day": 10,
+            "next_5_day": 6,
+            "next_6_day": 6,
+            "next_7_day": 6,
+            "next_8_day": 4,
+            "next_9_day": 6,
+            "next_10_day": 4
         },
         {
-            "next_2_day": 12,
-            "next_3_day": 12,
-            "next_4_day": 12,
-            "next_5_day": 12,
-            "next_6_day": 12,
-            "next_7_day": 12,
-            "next_8_day": 12,
-            "next_9_day": 12,
-            "next_10_day": 12
+            "next_2_day": 8,
+            "next_3_day": 4,
+            "next_4_day": 1,
+            "next_5_day": 3,
+            "next_6_day": 2,
+            "next_7_day": 2,
+            "next_8_day": 3,
+            "next_9_day": 3,
+            "next_10_day": 2
         }
     ]
 
-    data_low_low = data[(result["open"]["next_1_day"] < result["close"]["next_0_day"]) & (result["close"]["next_1_day"] < result["close"]["next_0_day"])]
-    data_low_high = data[(result["open"]["next_1_day"] < result["close"]["next_0_day"]) & (result["close"]["next_1_day"] > result["close"]["next_0_day"])]
-    data_high_low = data[(result["open"]["next_1_day"] > result["close"]["next_0_day"]) & (result["close"]["next_1_day"] < result["close"]["next_0_day"])]
-    data_high_high = data[(result["open"]["next_1_day"] > result["close"]["next_0_day"]) & (result["close"]["next_1_day"] > result["close"]["next_0_day"])]
+    data_low_low = data[
+        (result["open"]["next_1_day"] < result["close"]["next_0_day"]) & (result["close"]["next_1_day"] < result["close"]["next_0_day"])]
+    data_low_high = data[
+        (result["open"]["next_1_day"] < result["close"]["next_0_day"]) & (result["close"]["next_1_day"] > result["close"]["next_0_day"])]
+    data_high_low = data[
+        (result["open"]["next_1_day"] > result["close"]["next_0_day"]) & (result["close"]["next_1_day"] < result["close"]["next_0_day"])]
+    data_high_high = data[
+        (result["open"]["next_1_day"] > result["close"]["next_0_day"]) & (result["close"]["next_1_day"] > result["close"]["next_0_day"])]
 
-    for point_tendency_type_str, data, max_depth_map in zip(["data_low_low", "data_low_high", "data_high_low", "data_high_high"], [data_low_low, data_low_high, data_high_low, data_high_high], max_depth_map_list):
+    for point_tendency_type_str, data, max_depth_map in zip(["data_low_low", "data_low_high", "data_high_low", "data_high_high"],
+                                                            [data_low_low, data_low_high, data_high_low, data_high_high],
+                                                            max_depth_map_list):
         print(point_tendency_type_str)
         percent_series = data["open"]["next_0_day"] / 100
 
@@ -196,30 +202,85 @@ def idea_07(data):
             forest_list.append([day, forest, X_train, X_test, y_train, y_test])
 
         for day, forest, X_train, X_test, y_train, y_test in forest_list:
-            if forest.score(X_train, y_train) > 0.7 and forest.score(X_test, y_test) > 0.7:
-
+            if forest.score(X_train, y_train) > 0.71 and forest.score(X_test, y_test) > 0.71:
                 print("day is {0}".format(day))
                 print("Accuracy on training set: {:.3f}".format(forest.score(X_train, y_train)))
                 print("Accuracy on test set: {:.3f}".format(forest.score(X_test, y_test)))
 
-                # all_ts_code = get_result_ts_code_list()
-                # date = datetime.datetime(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day)
-                # for ts_code in all_ts_code:
-                #     security_point_data = SecurityData().get_security_point_data(ts_code, date, date)
-                #     if security_point_data.empty is False:
-                #         security_point_data = security_point_data.loc[date]
-                #     else:
-                #         continue
-                #
-                #     predict_data = [security_point_data["open"], security_point_data["close"], security_point_data["high"],
-                #                     security_point_data["low"]]
-                #     predict_data = np.array([predict_data]) / (predict_data[0] / 100)
-                #     print(ts_code, forest.predict(predict_data))
-                #
-                # print("\n\n\n\n\n")
+                all_ts_code = get_result_ts_code_list()
+                date_now = datetime.datetime.now() - datetime.timedelta(days=1)
+                date = datetime.datetime(date_now.year, date_now.month, date_now.day)
+                for ts_code in all_ts_code:
+                    security_point_data = SecurityData().get_security_point_data(ts_code, date, date)
+                    if security_point_data.empty is False:
+                        security_point_data = security_point_data.loc[date]
+                    else:
+                        continue
+
+                    predict_data = [security_point_data["open"], security_point_data["close"], security_point_data["high"],
+                                    security_point_data["low"]]
+                    predict_data = np.array([predict_data]) / (predict_data[0] / 100)
+                    predict_result_now = forest.predict(predict_data)
+                    if predict_result_now == [True]:
+                        print(ts_code, predict_result_now)
+
+                print("\n\n")
+        print("\n\n\n")
+
+
+def idea_08(data):
+    """
+    测试不同max_depth的表现
+    """
+    data_low_low = data[
+        (result["open"]["next_1_day"] < result["close"]["next_0_day"]) & (result["close"]["next_1_day"] < result["close"]["next_0_day"])]
+    data_low_high = data[
+        (result["open"]["next_1_day"] < result["close"]["next_0_day"]) & (result["close"]["next_1_day"] > result["close"]["next_0_day"])]
+    data_high_low = data[
+        (result["open"]["next_1_day"] > result["close"]["next_0_day"]) & (result["close"]["next_1_day"] < result["close"]["next_0_day"])]
+    data_high_high = data[
+        (result["open"]["next_1_day"] > result["close"]["next_0_day"]) & (result["close"]["next_1_day"] > result["close"]["next_0_day"])]
+
+    data = data_high_high
+
+    percent_series = data["open"]["next_0_day"] / 100
+
+    target_day_list = ["next_2_day", "next_3_day", "next_4_day", "next_5_day", "next_6_day", "next_7_day", "next_8_day", "next_9_day",
+                       "next_10_day"]
+    target_data_list = []
+    for day in target_day_list:
+        target_data_list.append([day, (data["close"][day] > data["high"]["next_1_day"]).values])
+    data = pd.DataFrame({
+        "open": data["open"]["next_0_day"] / percent_series,
+        "close": data["close"]["next_0_day"] / percent_series,
+        "high": data["high"]["next_0_day"] / percent_series,
+        "low": data["low"]["next_0_day"] / percent_series
+    }).values
+
+    for day, target_data in target_data_list:
+        print("day is {0}".format(day))
+        depth_list = []
+        training_score_list = []
+        test_score_list = []
+        for max_depth in range(1, 31):
+            X_train, X_test, y_train, y_test = train_test_split(data, target_data, random_state=0)
+            forest = RandomForestClassifier(n_estimators=100, random_state=0, max_depth=max_depth)
+            forest.fit(X_train, y_train)
+            print("Accuracy on training set: {:.3f}".format(forest.score(X_train, y_train)))
+            print("Accuracy on test set: {:.3f}".format(forest.score(X_test, y_test)))
+            training_score_list.append(forest.score(X_train, y_train))
+            test_score_list.append(forest.score(X_test, y_test))
+            depth_list.append(max_depth)
+
+        plt.figure(figsize=(22, 15))
+        plt.locator_params(nbins=30, axis='x')
+        plt.plot(depth_list, training_score_list, ls="-", lw=2, label="training figure")
+        plt.plot(depth_list, test_score_list, ls="-", lw=2, label="test figure")  # ls: 折线图线条风格, lw: 线条宽度, label: 标签文本
+        plt.title(day)
+        plt.legend()  # 用以显示label的内容
+        plt.show()
         print("\n\n\n\n\n")
 
-# def idea_08(data):
 
 def start(data):
     # idea_01(data)
@@ -228,6 +289,7 @@ def start(data):
     # idea_04(data)
     # idea_05(data)
     idea_07(data)
+    # idea_08(data)
 
 
 if __name__ == "__main__":
