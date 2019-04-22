62e7c1bae3ae399639da5a1b24e0f25316fecf82
NewLanded <l1141041@163.com>
2019-04-22 17:56:58 +0800

    test something but failed

--- idea/penetrate_lower_bband/analyst_bbond_result.py
+++ idea/penetrate_lower_bband/analyst_bbond_result.py
@@ -121,48 +121,48 @@ def idea_07(data):
     """
     max_depth_map_list = [
         {
-            "next_2_day": 1,
-            "next_3_day": 13,
-            "next_4_day": 12,
-            "next_5_day": 9,
-            "next_6_day": 7,
-            "next_7_day": 8,
-            "next_8_day": 8,
-            "next_9_day": 7,
-            "next_10_day": 9
+            "next_2_day": 1,  # 0.92
+            "next_3_day": 9,  # 0.85
+            "next_4_day": 1,  # 0.825
+            "next_5_day": 9,  # 0.8
+            "next_6_day": 1,  # 0.76
+            "next_7_day": 8,  # 0.75
+            "next_8_day": 8,  # 0.75
+            "next_9_day": 7,  # 0.75
+            "next_10_day": 8  # 0.74
         },
         {
-            "next_2_day": 8,
-            "next_3_day": 6,
-            "next_4_day": 1,
-            "next_5_day": 6,
-            "next_6_day": 2,
-            "next_7_day": 3,
-            "next_8_day": 2,
-            "next_9_day": 3,
-            "next_10_day": 2
+            "next_2_day": 8,  # 0.6
+            "next_3_day": 1,  # 0.58
+            "next_4_day": 1,  # 0.56
+            "next_5_day": 6,  # 0.52
+            "next_6_day": 3,  # 0.51
+            "next_7_day": 3,  # 0.55
+            "next_8_day": 3,  # 0.58
+            "next_9_day": 3,  # 0.59
+            "next_10_day": 1  # 0.58
         },
         {
-            "next_2_day": 15,
-            "next_3_day": 2,
-            "next_4_day": 10,
-            "next_5_day": 6,
-            "next_6_day": 6,
-            "next_7_day": 6,
-            "next_8_day": 4,
-            "next_9_day": 6,
-            "next_10_day": 4
+            "next_2_day": 12,  # 0.95
+            "next_3_day": 1,  # 0.87
+            "next_4_day": 10,  # 0.85
+            "next_5_day": 1,  # 0.825
+            "next_6_day": 1,  # 0.80
+            "next_7_day": 1,  # 0.76
+            "next_8_day": 1,  # 0.77
+            "next_9_day": 1,  # 0.76
+            "next_10_day": 1  # 0.77
         },
         {
-            "next_2_day": 8,
-            "next_3_day": 4,
-            "next_4_day": 1,
-            "next_5_day": 3,
-            "next_6_day": 2,
-            "next_7_day": 2,
-            "next_8_day": 3,
-            "next_9_day": 3,
-            "next_10_day": 2
+            "next_2_day": 8,  # 0.74
+            "next_3_day": 4,  # 0.68
+            "next_4_day": 1,  # 0.61
+            "next_5_day": 5,  # 0.61
+            "next_6_day": 2,  # 0.58
+            "next_7_day": 2,  # 0.62
+            "next_8_day": 6,  # 0.67
+            "next_9_day": 3,  # 0.58
+            "next_10_day": 1  # 0.60
         }
     ]
 
@@ -241,7 +241,7 @@ def idea_08(data):
     data_high_high = data[
         (result["open"]["next_1_day"] > result["close"]["next_0_day"]) & (result["close"]["next_1_day"] > result["close"]["next_0_day"])]
 
-    data = data_high_high
+    data = data_low_low
 
     percent_series = data["open"]["next_0_day"] / 100
 
@@ -272,14 +272,46 @@ def idea_08(data):
             test_score_list.append(forest.score(X_test, y_test))
             depth_list.append(max_depth)
 
-        plt.figure(figsize=(22, 15))
+        # plt.figure(figsize=(22, 15))
+        plt.figure(figsize=(6, 6))
         plt.locator_params(nbins=30, axis='x')
         plt.plot(depth_list, training_score_list, ls="-", lw=2, label="training figure")
         plt.plot(depth_list, test_score_list, ls="-", lw=2, label="test figure")  # ls: 折线图线条风格, lw: 线条宽度, label: 标签文本
         plt.title(day)
         plt.legend()  # 用以显示label的内容
-        plt.show()
+        # plt.show()
+        plt.savefig('./day_' + str(day) + '.png')
         print("\n\n\n\n\n")
+
+
+def visualization_1(data):
+    """从结果来看, 没吊用"""
+    data_low_low = data[
+        (result["open"]["next_1_day"] < result["close"]["next_0_day"]) & (result["close"]["next_1_day"] < result["close"]["next_0_day"])]
+    data_low_high = data[
+        (result["open"]["next_1_day"] < result["close"]["next_0_day"]) & (result["close"]["next_1_day"] > result["close"]["next_0_day"])]
+    data_high_low = data[
+        (result["open"]["next_1_day"] > result["close"]["next_0_day"]) & (result["close"]["next_1_day"] < result["close"]["next_0_day"])]
+    data_high_high = data[
+        (result["open"]["next_1_day"] > result["close"]["next_0_day"]) & (result["close"]["next_1_day"] > result["close"]["next_0_day"])]
+
+    data = data_low_low
+
+    percent_series = data["open"]["next_0_day"] / 100
+
+    target_day_list = ["next_2_day"]
+    target_data_list = []
+    for day in target_day_list:
+        target_data_list.append([day, data["close"][day] > data["high"]["next_1_day"]])
+    data = pd.DataFrame({
+        "open": data["open"]["next_0_day"] / percent_series,
+        "close": data["close"]["next_0_day"] / percent_series,
+        "high": data["high"]["next_0_day"] / percent_series,
+        "low": data["low"]["next_0_day"] / percent_series
+    })
+
+    for day, target_data in target_data_list:  # 这里要debug着看, 不然不出图???, 可以试着把图保存下来再看
+        pd.scatter_matrix(data, c=target_data, figsize=(15, 15), marker='o', hist_kwds={'bins': 20}, s=60, alpha=0.8)
 
 
 def start(data):
@@ -288,8 +320,9 @@ def start(data):
     # idea_03(data)
     # idea_04(data)
     # idea_05(data)
-    idea_07(data)
-    # idea_08(data)
+    # idea_07(data)
+    idea_08(data)
+    # visualization_1(data)
 
 
 if __name__ == "__main__":
