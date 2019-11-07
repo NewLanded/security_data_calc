9610a7b3c638ce9a3b27bc8923c91a9e142fbb0f
NewLanded <l1141041@163.com>
2019-11-07 17:45:04 +0800

    optimize interface buy_when_holder_number_fall

--- strategy/buy/buy_when_holder_number_fall.py
+++ strategy/buy/buy_when_holder_number_fall.py
@@ -40,14 +40,15 @@ def buy(security_point_data, holder_number_data):
         holder_number_date_point = security_point_data.loc[security_point_data["trade_date"].apply(lambda x: x in holder_number_data_date)]
 
         if holder_number_data == sorted(holder_number_data, reverse=True):
-            if sum([security_point_data["close"].iloc[-1], security_point_data["close"].iloc[-2], security_point_data["close"].iloc[-3]]) > sum(
-                    [security_point_data["close"].iloc[-4],
-                     security_point_data["close"].iloc[-5],
-                     security_point_data["close"].iloc[-6]]):
-                if security_point_data["close"].iloc[-1] > holder_number_date_point['close'].iloc[-1]:
-                    if holder_number_date_point['close'].iloc[-1] < holder_number_date_point['close'][-2] < holder_number_date_point['close'][-3] < \
-                            holder_number_date_point['close'][-4]:
-                        return True
+            # if sum([security_point_data["close"].iloc[-1], security_point_data["close"].iloc[-2], security_point_data["close"].iloc[-3]]) > sum(
+            #         [security_point_data["close"].iloc[-4],
+            #          security_point_data["close"].iloc[-5],
+            #          security_point_data["close"].iloc[-6]]):  时间拖得太久了, 涨的时间段都已经过去了
+            if security_point_data["close"].iloc[-1] > security_point_data["close"].iloc[-2] and security_point_data["close"].iloc[-1] > security_point_data["close"].iloc[-3]:
+                # if security_point_data["close"].iloc[-1] > holder_number_date_point['close'].iloc[-1]:  没道理啊, 不一定披露日就是最低的点位
+                if holder_number_date_point['close'].iloc[-1] < holder_number_date_point['close'][-2] < holder_number_date_point['close'][-3] < \
+                        holder_number_date_point['close'][-4]:
+                    return True
 
     return False
 
@@ -63,7 +64,7 @@ def start(date_now=None):
     date_now = datetime.datetime(date_now.year, date_now.month, date_now.day)
 
     if Date().is_workday(date_now):
-        ts_codes = Security().get_efficient_ts_code_by_column_name_list(["normal_status", "tactics_5_status", "tactics_6_status"])
+        ts_codes = Security().get_efficient_ts_code_by_column_name_list(["normal_status", "tactics_5_status", "tactics_7_status"])
         start_date, end_date = date_now - datetime.timedelta(days=465), date_now
         for ts_code in ts_codes:
             # for ts_code in '000592.SZ', '603800.SH', '603700.SH', '002547.SZ', '000709.SZ', '002006.SZ', '002375.SZ', '002009.SZ', '002873.SZ', '002883.SZ', '002911.SZ', '002862.SZ', '601005.SH':
@@ -88,5 +89,5 @@ def start(date_now=None):
 
 
 if __name__ == "__main__":
-    start_date, end_date = datetime.datetime(2019, 9, 6), datetime.datetime(2016, 5, 8)
+    start_date, end_date = datetime.datetime(2019, 11, 6), datetime.datetime(2016, 5, 8)
     start(start_date)
