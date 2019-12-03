689c7e9b754dcd493ddf011eda7f3c3062c6e744
flydedog <l1141041@163.com>
2019-12-03 20:25:31 +0800

    optimize symbol code

--- util/util_base/constant.py
+++ util/util_base/constant.py
@@ -1,2 +1,3 @@
 symbol_endwith_L = ['L', 'AL']  # 以L结尾的代码, 主力连续的代码也以L结尾, 为了和这个区分开
-manual_code_list = ['CF', 'OI', 'FG', 'RM', 'CY', 'AP', 'SR', 'JD', 'C', 'L', 'M', 'P', 'Y', 'HC', 'RB']  # 决定交易的代码, 只算这些
+manual_code_list = ['CF', 'OI', 'FG', 'RM', 'CY', 'AP', 'SR', 'JD', 'C', 'M', 'P', 'Y', 'HC', 'RB', 'MA', 'EG', 'AG', 'PP', 'AU', 'A', 'CS',
+                    'SP', 'CJ']  # 决定交易的代码, 只算这些
